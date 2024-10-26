df3cd9b1967faf384a12f34cd8da053789081f38
rafnas-p <rafnasrooni@gmail.com>
2024-10-26 11:04:12 +0530

    Join Two Arrays by ID

--- LeatCod.js
+++ LeatCod.js
@@ -80,3 +80,40 @@ const obj = [null, 0, false, 1]
 
 const resulte=compactObject (obj)
 console.log(resulte)
+
+
+
+///////////////
+
+//2722. Join Two Arrays by ID
+
+var join = function(arr1, arr2) {
+    
+  const result={}
+  for(let i=0;i<arr1.length;i++){
+      result[arr1[i].id]=arr1[i]
+  }
+  for (let i=0;i<arr2.length;i++){
+    if(result[arr2[i].id]){
+        for(const key in arr2[i]){
+            result[arr2[i].id[key]=arr2[i][key]]
+        }
+        }else{
+             result[arr2[i].id]=arr2[i]
+        
+    }
+  }
+  
+  return Object.values(result)
+ }; 
+ 
+ const arr1 = [
+     {"id": 1, "x": 2, "y": 3},
+     {"id": 2, "x": 3, "y": 6}
+ ], 
+ arr2 = [
+     {"id": 2, "x": 10, "y": 20},
+     {"id": 3, "x": 0, "y": 0}
+ ]
+ const resulty =join(arr1,arr2)
+ console.log(resulty)
